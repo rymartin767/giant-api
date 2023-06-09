127bfa9a712bc3d3ba05d49113bb41a8a67cd9a4
Ryan Martin <ryan@nt4c.com>
2023-06-09 19:17:48 -0400

    JUN09 WIP

---
+++ app/Http/Livewire/Dashboard/Articles.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard;
+
+use App\Models\Article;
+use Livewire\Component;
+
+class Articles extends Component
+{
+    public $loaded = false;
+    public $status;
+
+    public function render()
+    {
+        return view('livewire.dashboard.articles');
+    }
+
+    public function initLoading() : void
+    {
+        $article = Article::latest()->get()->first();
+        $this->status = str($article->title)->limit(15) . ' published ' . $article->created_at->diffForHumans();
+
+        $this->loaded = true;
+    }
+}
