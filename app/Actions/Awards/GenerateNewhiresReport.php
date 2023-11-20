a538ea9cd1c7c6e8e4c21337fe7589925df7e83a
Ryan Martin <ryan@nt4c.com>
2023-11-19 20:53:57 -0500

    NOV19 WIP

--- app/Actions/Awards/GenerateNewhiresReport.php
+++ app/Actions/Awards/GenerateNewhiresReport.php
@@ -14,17 +14,15 @@ final readonly class GenerateNewhiresReport
 
         $newhires = $awards->where('is_new_hire', true)->groupBy('award_domicile');
 
-        return $newhires;
-
-        dd($newhires);
-
+        $domiciles = $newhires->keys();
+        
         $domiciles->each(function($base) use ($report, $awards) {
             $report->put($base, [
-                'total' => $awards->where('is_new_hire', true, 'award_domicile', $base)->count(),
-                '737' => $awards->where('is_new_hire', true, 'award_domicile', $base)->where('award_fleet', '737')->count(),
-                '747' => $awards->where('is_new_hire', true, 'award_domicile', $base)->where('award_fleet', '747')->count(),
-                '767' => $awards->where('is_new_hire', true, 'award_domicile', $base)->where('award_fleet', '767')->count(),
-                '777' => $awards->where('is_new_hire', true, 'award_domicile', $base)->where('award_fleet', '777')->count(),
+                'total' => $awards->where('award_domicile', $base)->count(),
+                '737' => $awards->where('award_domicile', $base)->where('award_fleet', '737')->count(),
+                '747' => $awards->where('award_domicile', $base)->where('award_fleet', '747')->count(),
+                '767' => $awards->where('award_domicile', $base)->where('award_fleet', '767')->count(),
+                '777' => $awards->where('award_domicile', $base)->where('award_fleet', '777')->count(),
             ]);
         });
 
--- tests/Feature/API/Award/NewhiresControllerTest.php
+++ tests/Feature/API/Award/NewhiresControllerTest.php
@@ -35,6 +35,7 @@ it('returns a collection response', function() {
     Award::factory(5)->create(['award_domicile' => 'CVG', 'award_fleet' => '767', 'is_new_hire' => true]);
     Award::factory(5)->create(['award_domicile' => 'CVG', 'award_fleet' => '777', 'is_new_hire' => true]);
     Award::factory(8)->create(['award_domicile' => 'ONT', 'award_fleet' => '767', 'is_new_hire' => true]);
+    Award::factory(2)->create(['award_domicile' => 'TPA', 'award_fleet' => '767', 'is_new_hire' => false]);
 
     $this->actingAs(sanctumToken())->get('v1/awards/newhires')
         ->assertExactJson([
@@ -63,4 +64,4 @@ it('returns a collection response', function() {
             ]
         ])
         ->assertOk();
-})->todo();
+});
