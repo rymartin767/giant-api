379609a367b2fc72b4b2a8e2a9e205f37d554975
Ryan Martin <ryan@nt4c.com>
2024-03-21 09:24:51 -0400

    MAR21 WIP

--- app/Actions/Awards/GenerateJuniorsReport.php
+++ app/Actions/Awards/GenerateJuniorsReport.php
@@ -22,6 +22,11 @@ final readonly class GenerateJuniorsReport
         }
     }
 
+    private function month() : string
+    {
+        return $this->awards->first()->month->format('F Y');
+    }
+
     private function positions()
     {
         return collect([
@@ -69,18 +74,21 @@ final readonly class GenerateJuniorsReport
     private function firstOfficers() : Collection
     {
         $collection = collect();
+        $juniors = collect();
+        $collection->put('award_date', $this->month());
         
-        $this->positions()->map(function($position) use($collection) {
+        $this->positions()->map(function($position) use($juniors) {
             $base = explode(" ", $position)[0];
             $fleet = explode(" ", $position)[1];
 
-            $junior_in_base_award = $this->awards->where('award_domicile', $base)->where('award_seat', 'FO')->where('award_fleet', $fleet)->where('omit_from_juniors', false)->sortBy('base_seniority')->last();
+            $junior_in_base_award = $this->awards->where('award_domicile', $base)->where('award_seat', 'FO')->where('award_fleet', $fleet)->sortBy('base_seniority')->last();
 
             if (! $junior_in_base_award == null) {
-                $collection->put($position, $junior_in_base_award->is_new_hire ? 'New Hire' : Carbon::parse($junior_in_base_award->pilot->doh)->format('m/d/Y'));
+                $juniors->put($position, $junior_in_base_award->is_new_hire ? 'New Hire' : Carbon::parse($junior_in_base_award->pilot->doh)->format('m/d/Y'));
             };
         });
 
+        $collection->put('dohs', $juniors);
         $collection = $collection->sort();
         return $collection;
     }
